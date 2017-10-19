b16814639305f66ad8442a75a3f4901d72137b8b
Andrew Sayers <8bat@pileofstuff.org>
2017-10-19 10:38:15 +0100

    Remove redundant xmlns:xlink attribute

--- lib/TheInker.pm
+++ lib/TheInker.pm
@@ -2670,7 +2670,7 @@ sub render_svg {
     return <<END
 <?xml version="1.0" encoding="UTF-8" standalone="yes"?>
 <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.0//EN" "http://www.w3.org/TR/2001/REC-SVG-20010904/DTD/svg10.dtd">
-<svg viewBox="0 0 $svg_width $svg_height" style="fill-rule:evenodd;stroke-linecap:square;stroke-linejoin:round;stroke-width:$stroke_width" height="100%" width="100%" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+<svg viewBox="0 0 $svg_width $svg_height" style="fill-rule:evenodd;stroke-linecap:square;stroke-linejoin:round;stroke-width:$stroke_width" height="100%" width="100%" xmlns="http://www.w3.org/2000/svg">
 $defs$paths</svg>
 END
 ;
